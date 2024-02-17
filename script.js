54f5f33d37e93a41939d47a472261a1b2d969af2
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-02-16 20:29:17 -0500

    Created function to make grid of divs

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+function spawnGrid(input) {
+
+    let container = document.querySelector("#container");
+    for (let i = 0; i < input; i++) {
+        let column = document.createElement("div");
+        column.classList.add("column");
+
+    
+    for ( let k = 0; k <= input; k++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        row.style.border = "2px solid black";
+        column.appendChild(row)
+   
+    }
+       container.appendChild(column); 
+    }
+};
+
+
+
+
+spawnGrid(16);
+

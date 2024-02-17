449a590101aea03eaaaa565b8f352406d27578be
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-02-17 09:12:39 -0500

    created button, event listeners
    added button functions to create to grids and too delete old grid

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function spawnGrid(input) {
         column.classList.add("column");
 
     
-    for ( let k = 0; k <= input; k++) {
+    for ( let k = 0; k < input; k++) {
         let row = document.createElement("div");
         row.classList.add("row");
         row.style.border = "2px solid black";
@@ -22,7 +22,35 @@ function spawnGrid(input) {
 spawnGrid(16);
 
 let boxes = document.getElementsByClassName("row"); 
+const btn = document.querySelector("#btn");
 
+
+btn.addEventListener("click", () => {
+    let newGrid = prompt("Enter desired new grid, MAX:100");
+     let parent = document.querySelector("#container"); 
+     while(parent.lastChild) {
+         parent.lastChild.remove();
+     } 
+     if (newGrid >= 100) {
+        alert("TOO LARGE OF A NUMBER");
+        newGrid = prompt("Enter desired new grid, MAX:100");
+    } 
+     spawnGrid(newGrid);
+    
+
+
+     for(let box of boxes) { 
+
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "red";
+            console.log(box);
+        });
+        
+        
+        };
+        
+
+ });
 
 for(let box of boxes) { 
 
@@ -33,3 +61,4 @@ box.addEventListener("mouseover", () => {
 
 
 };
+

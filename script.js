09eb6b15516d9b21e77bc19bd53c257f3a584eca
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-02-18 11:02:02 -0500

    made toolbar and buttons

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+let boxes = document.getElementsByClassName("row"); 
+const btn = document.querySelector("#btn");
+btn.addEventListener("click",newGrid);
+const sprayTool = document.querySelector("#sprayCan");
+const penTool = document.querySelector("#pen");
+let enabled = true;
 
+   
+sprayTool.addEventListener("click", sprayCan);
+penTool.addEventListener("click", pen);
 
 function spawnGrid(input) {
     let container = document.querySelector("#container");
@@ -21,9 +30,21 @@ function spawnGrid(input) {
 };
 spawnGrid(16);
 
-let boxes = document.getElementsByClassName("row"); 
-const btn = document.querySelector("#btn");
-btn.addEventListener("click",newGrid);
+
+function newGrid() {
+    let newGrid = prompt("Enter desired new grid, MAX:100");
+    let parent = document.querySelector("#container"); 
+    while(parent.lastChild) {
+        parent.lastChild.remove();
+    } 
+    if (newGrid > 100) {
+       alert("NUMBER TOO LARGE");
+       newGrid = prompt("Enter desired new grid, MAX:100");
+        } else spawnGrid(newGrid);
+
+        sprayCan();
+};
+   
 
 function randomInteger(max) {
     return Math.floor(Math.random()*(max + 1));
@@ -48,30 +69,26 @@ function randomHexColor() {
 };
 
 
-function newGrid() {
-    let newGrid = prompt("Enter desired new grid, MAX:100");
-    let parent = document.querySelector("#container"); 
-    while(parent.lastChild) {
-        parent.lastChild.remove();
-    } 
-    if (newGrid > 100) {
-       alert("NUMBER TOO LARGE");
-       newGrid = prompt("Enter desired new grid, MAX:100");
-        } else spawnGrid(newGrid);
-
-        sprayCan();
-};
-   
-sprayCan();
-
-
 function sprayCan() {
+    
 for(let box of boxes) { 
-box.addEventListener("mouseover", () => {
-    let hex = randomHexColor();
-    box.style.backgroundColor = hex;
-});
+    box.addEventListener("mouseover", () => {
+        if (enabled == true) { 
+            
+    box.style.backgroundColor = "black";
+}})};
+};
 
-
-}};
-
+function pen() {
+    enabled = false;
+    for (let box of boxes) {
+        box.addEventListener("mousedown", () => {
+        box.style.backgroundColor = "black";
+        
+        })
+        sprayTool.addEventListener("mouseup", () => {
+            enabled = true
+        })
+    
+    }
+};
